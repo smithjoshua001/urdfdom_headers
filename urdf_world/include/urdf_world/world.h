a57a72e25b20183ed4a9ec668397150a07d2b9f2
Joshua Smith <smithjoshua001@aol.co.uk>
2017-11-30 16:39:16 +0000

    Adding tinyxml2 support

--- urdf_world/include/urdf_world/world.h
+++ urdf_world/include/urdf_world/world.h
@@ -1,13 +1,13 @@
 /*********************************************************************
 * Software License Agreement (BSD License)
-* 
+*
 *  Copyright (c) 2008, Willow Garage, Inc.
 *  All rights reserved.
-* 
+*
 *  Redistribution and use in source and binary forms, with or without
 *  modification, are permitted provided that the following conditions
 *  are met:
-* 
+*
 *   * Redistributions of source code must retain the above copyright
 *     notice, this list of conditions and the following disclaimer.
 *   * Redistributions in binary form must reproduce the above
@@ -17,7 +17,7 @@
 *   * Neither the name of the Willow Garage nor the names of its
 *     contributors may be used to endorse or promote products derived
 *     from this software without specific prior written permission.
-* 
+*
 *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -73,7 +73,7 @@
 #include <string>
 #include <vector>
 #include <map>
-
+#include <tinyxml2.h>
 #include "urdf_model/model.h"
 #include "urdf_model/pose.h"
 #include "urdf_model/twist.h"
@@ -99,6 +99,8 @@ public:
 
   std::vector<Entity> models;
 
+  void initXml(tinyxml2::XMLElement* config);
+
   void clear()
   {
     this->name.clear();
@@ -107,4 +109,3 @@ public:
 }
 
 #endif
-
